46f6842e30563d12634619a15b307528725025e8
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-03-20 23:34:10 +0330

    [catalogue] Materialize command package

--- services/catalogue/cmd/api/command/run.go
+++ services/catalogue/cmd/api/command/run.go
@@ -3,26 +3,38 @@ package command
 import (
 	"flag"
 	"fmt"
-	"os"
 
+	"github.com/hossein1376/BehKhan/catalogue/internal/application/controller"
+	"github.com/hossein1376/BehKhan/catalogue/internal/infrastructure/database/maria/pool"
 	"github.com/hossein1376/BehKhan/catalogue/internal/interface/config"
 	"github.com/hossein1376/BehKhan/catalogue/internal/interface/rest"
 )
 
-func Run() {
+func Run() error {
 	var configPath string
 	flag.StringVar(&configPath, "cfg", "assets/config/sample.yaml", "Configuration File")
 	flag.Parse()
 
 	c, err := config.Load(configPath)
 	if err != nil {
-		fmt.Fprintf(os.Stdout, "load configs: %v", err)
-		os.Exit(1)
+		return fmt.Errorf("load configs: %w", err)
 	}
 
-	err = rest.Start(c.Rest.Port)
+	dsn := fmt.Sprintf("%s:%s@/%s", c.DB.Username, c.DB.Password, c.DB.Name)
+	db, err := pool.New(dsn)
 	if err != nil {
-		fmt.Fprintf(os.Stdout, "start rest server: %v", err)
-		os.Exit(1)
+		return fmt.Errorf("open database connection: %w", err)
 	}
+	ctrl := controller.New(db)
+
+	// mount routes
+	// ...
+	_ = ctrl
+
+	err = rest.Start(c.Rest.Addr)
+	if err != nil {
+		return fmt.Errorf("start rest server: %w", err)
+	}
+
+	return nil
 }
--- services/catalogue/cmd/api/main.go
+++ services/catalogue/cmd/api/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/hossein1376/BehKhan/catalogue/cmd/api/command"
 )
 
 func main() {
-	command.Run()
+	err := command.Run()
+	if err != nil {
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
 }
