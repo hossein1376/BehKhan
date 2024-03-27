a2d9c716f83ef63972910006918e076d1fa7432f
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-03-27 21:49:00 +0330

    [catalogue] Add grpc to configurations

--- services/catalogue/internal/interface/config/cfg1/cfg1.go
+++ services/catalogue/internal/interface/config/cfg1/cfg1.go
@@ -20,9 +20,14 @@ type rest struct {
 	Addr string `yaml:"addr"`
 }
 
+type grpc struct {
+	Addr string `yaml:"addr"`
+}
+
 type Config struct {
 	DB   db   `yaml:"db"`
 	Rest rest `yaml:"rest"`
+	Grpc grpc `yaml:"grpc"`
 }
 
 func (c Config) Validate() error {
@@ -40,6 +45,9 @@ func (c Config) Validate() error {
 	}
 	if c.Rest.Addr == "" {
 		return fmt.Errorf("empty Rest address")
+	}
+	if c.Grpc.Addr == "" {
+		return fmt.Errorf("empty gRPC address")
 	}
 	if _, err := strconv.Atoi(c.DB.Port); err != nil {
 		return fmt.Errorf("invalid db port: %s", c.DB.Port)
