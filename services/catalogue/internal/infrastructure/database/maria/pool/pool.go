1e205ea1622e888dd9740bfd17d73078e927131b
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-03-31 15:59:17 +0330

    [catalogue] Add documentation to pool package

--- services/catalogue/internal/infrastructure/database/maria/pool/pool.go
+++ services/catalogue/internal/infrastructure/database/maria/pool/pool.go
@@ -10,17 +10,24 @@ import (
 	"github.com/hossein1376/BehKhan/catalogue/internal/infrastructure/database/maria"
 )
 
+// Pool embeds an instance of *maria.Tables, offering available repository methods. Also, due to
+// embedding *sql.Tx, it's possible to directly run raw sql statements against the database.
 type Pool struct {
 	*maria.Tables
 	*sql.Tx
 }
 
+// Query is an alias for closure function type to run against DB instances to query database using
+// repository methods or direct sql.
 type Query = func(ctx context.Context, r *Pool) error
 
+// DB contains an instance of *sql.DB. It is safe for concurrent use by multiple goroutines and
+// maintains its own pool of idle connections.
 type DB struct {
 	db *sql.DB
 }
 
+// New opens a database connection to a MariaDB (or MySQL) database with the given dsn address.
 func New(dsn string) (*DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
@@ -34,6 +41,8 @@ func New(dsn string) (*DB, error) {
 	return &DB{db: db}, nil
 }
 
+// Query starts a transaction and executes the given function. It creates and passes an instance of
+// Pool to the function. In case of an error, it will attempt to roll back.
 func (p *DB) Query(ctx context.Context, f Query) error {
 	tx, err := p.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -54,6 +63,7 @@ func (p *DB) Query(ctx context.Context, f Query) error {
 	return nil
 }
 
+// GetDB returns the underlying *sql.DB struct.
 func (p *DB) GetDB() *sql.DB {
 	return p.db
 }
