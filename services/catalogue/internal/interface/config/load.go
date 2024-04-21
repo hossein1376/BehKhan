0b30252162b6de193b4f5143eb0b03c31d8f529b
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2024-04-21 15:50:00 +0330

    [catalogue] Return a copy of configs to avoid accidental mutation

--- services/catalogue/internal/interface/config/load.go
+++ services/catalogue/internal/interface/config/load.go
@@ -7,29 +7,31 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func Load(path string) (*Config, error) {
+// Load reads the config file at the given path, parse its content and validate the configurations.
+func Load(path string) (Config, error) {
 	f, err := os.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("reading file: %w", err)
+		return Config{}, fmt.Errorf("reading file: %w", err)
 	}
 
 	c, err := Parse(f)
 	if err != nil {
-		return nil, fmt.Errorf("parsing configs: %w", err)
+		return Config{}, fmt.Errorf("parsing configs: %w", err)
 	}
 
 	if err = c.Validate(); err != nil {
-		return nil, fmt.Errorf("validating configs: %w", err)
+		return Config{}, fmt.Errorf("validating configs: %w", err)
 	}
 
 	return c, nil
 }
 
-func Parse(data []byte) (*Config, error) {
-	c := &Config{}
-	err := yaml.Unmarshal(data, c)
+// Parse reads the yaml data into a Config struct. It does not perform any validations on the configurations themselves.
+func Parse(data []byte) (Config, error) {
+	c := Config{}
+	err := yaml.Unmarshal(data, &c)
 	if err != nil {
-		return nil, fmt.Errorf("parsing yaml file: %w", err)
+		return Config{}, fmt.Errorf("parsing yaml file: %w", err)
 	}
 	return c, nil
 }
